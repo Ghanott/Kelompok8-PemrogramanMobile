e6cb4bb18cf989551cc0202d9098c6c0102778d8
Ghanott <ghanifadillah20@gmail.com>
2025-11-25 14:27:37 +0700

    menambahkan halaman fragment_chart.xml dan fragment_home.xml. sekaligus menambahkan di MainActivity.kt

--- app/src/main/java/com/example/aplikasiayamgeprek/home/MainActivity.kt
+++ app/src/main/java/com/example/aplikasiayamgeprek/home/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.aplikasiayamgeprek.LoginActivity
 import com.example.aplikasiayamgeprek.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +22,8 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         val username : String = intent.getStringExtra(LoginActivity.Companion.KEY_EMAIL).toString()
-
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragmentContainerView)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
