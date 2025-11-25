dda93358439bcd6f098627e5d0729ed1c42058b3
ok112r <yogoprasetyo2023@gmail.com>
2025-11-25 15:26:20 +0700

    menambahkan fragment_history.xml dan fragment_profile.xml

---
+++ app/src/main/java/com/example/aplikasiayamgeprek/home/HistoryFragment.kt
@@ -0,0 +1,36 @@
+package com.example.aplikasiayamgeprek.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.aplikasiayamgeprek.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HistoryFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HistoryFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_history, container, false)
+        val title = view.findViewById<TextView>(R.id.textViewHistory)
+        title.text = "ini adalah halaman history"
+        return view
+
+    }
+
+
+}
